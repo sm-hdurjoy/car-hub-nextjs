9fec0fa41ae3dba752386da4158a0bff64466546
sm-hdurjoy <smhazzazdurjoy@gmail.com>
2024-04-27 14:20:27 +0600

    initial commit, layout, header, footer, hero section done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,44 @@
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+"use client";
 
-export default Hero
+import CustomButton from "./CustomButton";
+import Image from "next/image";
+
+const Hero = () => {
+  const handleScroll = () => {};
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, Book, or rent a car -- quickly and easily!
+        </h1>
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with out effortless booking
+          process.
+        </p>
+
+        <CustomButton
+          title="Explore Parts"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/hero.png"
+            alt="hero"
+            fill
+            className="object-contain"
+            priority={true}
+          />
+        </div>
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
